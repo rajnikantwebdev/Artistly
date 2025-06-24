496ffd31b208af4d49cb57823df334f40da98421
rajnikantwebdev <rajnikantdevep@gmail.com>
2025-06-24 12:12:51 +0530

    added logic for removing filters

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,30 +1,55 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Button } from "@/components/ui/button";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
 } from "@/components/ui/dropdown-menu";
+import { Trash2 } from "lucide-react";
 
-const Filters = ({title, filtersValue, position, setPosition}) => {    
+const Filters = ({title, filtersValue, position, setPosition}) => {  
+    
+  const handleFilterRemove = (e) => {
+    e.stopPropagation();
+    setPosition(null);
+  };
     return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">{title}</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 bg-white">
-          <DropdownMenuSeparator />
-          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-            {filtersValue.map((filter, index) => <DropdownMenuRadioItem className="hover:bg-gray-200 cursor-pointer" key={index} value={filter} >{filter}</DropdownMenuRadioItem>)}
-          </DropdownMenuRadioGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="relative inline-flex items-center gap-1">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              {position === null ? title : position}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56 bg-white">
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup
+              value={position}
+              onValueChange={setPosition}
+            >
+              {filtersValue.map((filter, index) => (
+                <DropdownMenuRadioItem
+                  className="hover:bg-gray-200 cursor-pointer"
+                  key={index}
+                  value={filter}
+                >
+                  {filter}
+                </DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {position !== null && (
+          <Trash2
+            className="w-6 h-6 text-red-500 cursor-pointer"
+            onClick={handleFilterRemove}
+          />
+        )}
+      </div>
     );
 }
 
