471c137d91264a09116004d05ceb832991964551
rajnikantwebdev <rajnikantdevep@gmail.com>
2025-06-24 10:40:48 +0530

    env added

--- app/artists/page.jsx
+++ app/artists/page.jsx
@@ -4,7 +4,9 @@ import ArtistPage from '@/components/ArtistContainer'
 const page = async () => {
     let artistData = [];
     try {
-      const response = await fetch("http://localhost:3000/artistData.json");
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL}/artistData.json`
+      );
       artistData = await response.json();
     } catch (error) {
       console.log("fetch failed, try again later");
